b7226866a2359dfe37772128bced49adf085ebc6
isharamygina <121282432+isharamygina@users.noreply.github.com>
2023-12-09 23:23:31 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,45 @@
 // реализация пользовательского приложения
 
+#include "arithmetic.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+
 int main()
 {
-  return 0;
+	try
+	{
+		cout << "Hello! It`s your new calculator!" << endl;
+		cout << "You can use operations +,-,/,*. If \"-\" is unary, use breckets for correct work." << endl;
+		cout << "Names of your variables can use letters, digits (not on the first position) and \"_\"." << endl << endl;
+		cout << "Give the expression without spaces: ";
+		string exp;
+		cin >> exp;
+		Arithmetic expression(exp);
+		while (true)
+		{
+			int choice;
+			cout << "Do you want to calculate? \n1. Yes 2. New expression 3. Exit " << endl;
+			cin >> choice;
+			if (choice == 1)
+			{
+				double res;
+				res = expression.calculate();
+				cout << "The result  is: " << res << endl;;
+			}
+			if (choice == 2)
+			{
+				cout << "Give the expression: ";
+				cin >> exp;
+				Arithmetic expression(exp);
+			}
+			if (choice == 3)
+				break;
+			
+		}
+	}
+	catch (exception& e) {
+		cout << e.what();
+	}
 }
