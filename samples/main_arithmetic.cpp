bbe5d8e6a647869c4e80d9c4716e8ead04c85238
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2023-12-10 23:23:49 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,32 @@
 // реализация пользовательского приложения
+#include "arithmetic.h"
 
 int main()
 {
-  return 0;
+    cout << "Welcome to calculate!\n"<<
+           "You can use unar minus, +, -, *, / , (, ), sin(), cos(), tg(), exp(), lg(). YOU CAN'T USE SPACES!\n"<<
+           "So, you can give me as much as you want variables, but then you should give me their value\n"<<
+           "Good luck!" << endl;
+    cout << "Enter the equation:" << endl;
+    string equation;
+    double result;
+    cin >> equation;
+    Arithmetic res (equation);
+    //cout << "Enter the values of variables: " << endl;  //ПРОВЕРИТЬ МАПУ НА ПУСТОТУ И В ЭТОМ СЛУЧАЕ ЭТО
+    res.GetVars();
+    result = res.Calculate();
+    cout << result << endl;
+    while (true){
+        char choice;
+        cout << "Do you want to change the parameters and recalculate the equation? Y/N: ";
+        cin >> choice;
+        if (choice == 'N'){
+            break;
+        }
+        //cout << "Enter the values of variables: " << endl;
+        res.GetVars();
+        result = res.Calculate();
+        cout << result << endl;
+    }
+
 }
