1e1b078b20a954aabad5b75173cfb802fe7892ec
KorotinEgor <251251251azz@gmail.com>
2023-12-10 15:08:34 +0300

    comments

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -9,17 +9,17 @@
 class TPostfix {
 	std::vector<std::string> RPN;
 	
-	std::string Error_string(const std::string& s, int i); //Метод выделяющий первый ошибочный элемент в строке
-	
-	int get_prior(const std::string& s) noexcept; //Получить приоритет операции, если это не операция, возвращает 0
-	
-	int TPostfix::get_prior(char c) noexcept; //аналогичная функция для символов
+	std::string Error_string(const std::string& s, int i); //Method that selects element number "i" in a string. Uses only for selecting incorrect element when throws. 
 
-	double valid(const std::string& s); //Метод, превращающий строку, которая уже проверена на корректность, в число
+	int get_prior(const std::string& s) noexcept; //Method that return the priority of an operation. If it is not an operation returns 0
 	
-	std::string number_check(const std::string& s, int& i); //Метод принимает обрабатываемую строку и индекс, с которого начинается число и проверяет его на корректность, возвращая его
+	int TPostfix::get_prior(char c) noexcept; //similar method for characters
+
+	double valid(const std::string& s); //Method that turns correct string into a number
+	
+	std::string number_check(const std::string& s, int& i); //Method takes the string being processed and the index from which the expected number begins and checks it for correctness
 public:
-	TPostfix(const std::string& s); //Преобразовать строку в постфиксную форму записи, если это возможно
+	TPostfix(const std::string& s); //Turns string into a Reverse Polish Notation, if it possible.
 	
-	double count(); //Посчитать строку, хранящуюся в элементе класса
+	double count(); //Count a string stored in a class element
 };
