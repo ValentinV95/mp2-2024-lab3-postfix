e3526bcf0e892ab78e3b449907a3991a60694e14
isharamygina <121282432+isharamygina@users.noreply.github.com>
2023-12-12 15:55:06 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -8,38 +8,42 @@
 
 int main()
 {
-	try
-	{
+	
 		cout << "Hello! It`s your new calculator!" << endl;
 		cout << "You can use operations +,-,/,*. If \"-\" is unary, use breckets for correct work." << endl;
-		cout << "Names of your variables can use letters, digits and dotes (not on the first position) and \"_\"." << endl << endl;
-		cout << "Give the expression without spaces: ";
+		cout << "Names of your variables can use letters, digits and dots (not on the first position) and \"_\"." << endl;
+		cout << "Double numbers should use dot instead of comma, can use form with \'e\'" << endl << endl;
+		
 		string exp;
-		cin >> exp;
-		Arithmetic expression(exp);
 		while (true)
 		{
-			int choice;
-			cout << "Do you want to calculate? \n1. Yes 2. New expression 3. Exit " << endl;
-			cin >> choice;
-			if (choice == 1)
+			try
 			{
-				double res;
-				res = expression.calculate();
-				cout << "The result  is: " << res << endl;;
-			}
-			if (choice == 2)
-			{
-				cout << "Give the expression: ";
+				cout << "Give the expression without spaces: ";
 				cin >> exp;
 				Arithmetic expression(exp);
-			}
-			if (choice == 3)
-				break;
+
+				int choice;
+				cout << "Do you want to calculate? \n1. Yes 2. New expression 3. Exit " << endl;
+				cin >> choice;
+				while (choice==1)
+				{
+					double res;
+					res = expression.calculate();
+					cout << "The result  is: " << res << endl << endl;
+					cout << "Do you want to calculate? \n1. Yes 2. New expression 3. Exit " << endl;
+					cin >> choice;
+				}
+
+				if (choice == 2)
+					continue;
 			
+				if (choice == 3)
+					break;
+			}
+			catch (exception& e) {
+				cout << e.what() << endl << endl;
+			}
 		}
-	}
-	catch (exception& e) {
-		cout << e.what();
-	}
+
 }
