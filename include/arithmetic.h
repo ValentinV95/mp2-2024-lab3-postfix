308bbd625c1862213e3959e289a5d6ee867e6d28
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-12-19 13:10:12 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,34 @@
-// объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include "stack.h"
+
+using namespace std;class TPostfix
+{
+    string infix, postfix;
+    string operands;
+    string* var;
+    string* setOfLexems;
+    double* varValue;
+    int lexemSize;
+    int varSize;
+
+public:
+    ~TPostfix();
+    TPostfix(string& str);
+    void setInfix(string& str);
+    string getInfix() const;
+    string getPostfix() const;
+    string getOperands() const;
+    int priority(string operation);
+    bool isOperation(const char& x);
+    bool isOperand(const char& symbol);
+    bool isDigitOrPoint(const char& symbol);
+    void toLexems();
+    void check();
+    void toPostfix();
+    double toDouble(string number);
+    void valueofvars();
+    double calculate();
+};
