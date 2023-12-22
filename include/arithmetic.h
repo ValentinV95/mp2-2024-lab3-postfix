2cd0683951ae74580bd66a3bb877b7146b011dd7
Andrey Kirillov <kirillov.andrey.123.15@gmail.com>
2023-12-23 01:15:14 +0300

    add lab

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,34 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stack.h"
+#include <map>
+
+class TPostfix {
+
+	string infix;
+	vector<string> postfix;
+
+	int priority_of_operations(char op);
+	int priority_of_operations(const string& op);
+	void checking_brackets(const std::string& s) const;
+	string check_string_is_a_number(const string& str, size_t start_number, size_t end_number);
+	double string_to_number(const string& s_str);	
+public:
+	TPostfix(string str_infix)
+	{
+		infix = str_infix;
+	}
+	string get_infix()
+	{
+		return infix;
+	}
+	vector<string> get_postfix()
+	{
+		return postfix;
+	}
+	vector<string> to_postfix();
+	double calculate(map<string, double>& variables);
+};
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,71 @@
 
 #include "stack.h"
 #include <gtest.h>
+
+TEST(TStack, can_create_stack)
+{
+	ASSERT_NO_THROW(TStack<int> stack);
+}
+
+TEST(TStack, can_push_element_in_stack) {
+	TStack<int> stack;
+	ASSERT_NO_THROW(stack.push(123));
+}
+
+TEST(TStack, can_pop_element_from_stack) {
+	TStack<int> stack;
+	stack.push(123);
+	EXPECT_EQ(123, stack.pop());
+}
+
+TEST(TStack, isEmpty_is_correct) {
+	TStack<int> stack;
+	EXPECT_EQ(true, stack.isEmpty());
+}
+
+TEST(TStack, top_is_correct) {
+	TStack<int> stack;
+	stack.push(123);
+	EXPECT_EQ(123, stack.top());
+}
+
+TEST(TStack, can_top) {
+	TStack<int> stack;
+	stack.push(123);
+	ASSERT_NO_THROW(stack.top());
+}
+
+TEST(TStack, pop_delete_element_on_the_top_of_stack)
+{
+	TStack<int> stack;
+	stack.push(123);
+	stack.push(45);
+	stack.pop();
+	EXPECT_EQ(123, stack.pop());
+}
+
+TEST(TStack, throws_when_do_top_when_stack_is_empty) {
+	TStack<int> stack;
+	ASSERT_ANY_THROW(stack.top());
+}
+
+TEST(TStack, can_pop) {
+	TStack<int> stack;
+	stack.push(123);
+	ASSERT_NO_THROW(stack.pop());
+}
+
+TEST(TStack, throws_when_element_pop_from_empty_stack)
+{
+	TStack<int> stack;
+	stack.push(123);
+	stack.pop();
+	ASSERT_ANY_THROW(stack.pop());
+}
+
+TEST(TStack, stack_size_)
+{
+	TStack<int> stack;
+	stack.push(123);
+	ASSERT_EQ(1, stack.get_size());
+}
