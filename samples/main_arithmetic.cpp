952eaa118b52263697b122f2b23ca9c6e89122e2
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-12-25 02:33:47 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -36,7 +36,7 @@ int main()
             if (choice == 1)
             {
                TPostfix mathexp(line);
-               double res = mathexp.calculate();
+               double res = getResult.calculate();
                 cout << "The result: " << res << "\n";
             }
             if (choice == 2)
