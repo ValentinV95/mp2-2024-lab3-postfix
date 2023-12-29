879eea5be8b911fd812dabde06761899524b6f84
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-12-29 08:59:00 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -3,8 +3,6 @@
 
 int main()
 {
-    int main()
-{
     cout << "Hello, before you will use this calculator, you should read some rules: \n";
     cout << "You can use 4 operations: addition +, subsrtaction -, multiplication *, division / \n";
     cout << "You can denote vars by letters, digits and points \n";
@@ -12,14 +10,26 @@ int main()
     cout << "You should put a point instead of a comma in floating point numbers \n";
     cout << "You should avoid extra or misssing elements, be careful \n ";
     cout << endl;
-    string expression;
     cout << "Enter the arifmetic expression: \n";
-    cin >> expression;
+    string expression;
+    string error = "";
     while (true)
     {
         try
         {
             int choice;
+            cout << "Enter the arifmetic expression: \n";
+            getline(cin, expression);
+            for (int i = 0; i < expression.length(); i++)
+            {
+                error += expression[i];
+                if (expression[i] == ' ')
+                {
+                    error += " <- You shouldn`t use spaces";
+                    throw invalid_argument(error.c_str());
+                }
+            }
+            TPostfix mathexp(expression);
             cout << "Do you want to calculate? \n";
             cout << "1. Yes \n";
             cout << "2. Again \n";
@@ -27,17 +37,21 @@ int main()
             cin >> choice;
             if (choice == 1)
             {
-                TPostfix mathexp(expression);
                 cout << "Infix form: " << mathexp.getInfix() << "\n";
                 mathexp.toPostfix();
                 cout << "Postfix form: " << mathexp.getPostfix() << "\n";
                 mathexp.calculate();
                 cout << "The result: " << mathexp.getResult() << "\n";
+                cout << "Do you want to calculate? \n";
+                cout << "2. Again \n";
+                cout << "3. No \n";
+                cin >> choice;
             }
             if (choice == 2)
             {
                 cout << "Enter the arifmetic expression: \n";
-                cin >> expression;
+                getline(cin, expression);
+                continue;
             }
             if (choice == 3)
             {
@@ -49,6 +63,8 @@ int main()
             cout << e.what() << endl << endl;
         }
     }
+    return 0;
+}
     return 0;
 }
 }
