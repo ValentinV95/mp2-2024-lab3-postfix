effd55a1a3d017e307b314da30b284eadab52bd2
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-12-29 04:12:25 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -5,28 +5,18 @@ int main()
 {
     cout << "Hello, before you will use this calculator, you should read some rules: \n";
     cout << "You can use 4 operations: addition +, subsrtaction -, multiplication *, division / \n";
-    cout << "Don't use spaces in your line \n";
     cout << "You can denote vars by letters, digits and points \n";
     cout << "You should put a point instead of a comma in floating point numbers \n";
     cout << "You should avoid extra or misssing elements, be careful \n ";
     cout << endl;
-    string line;
+    string expression;
     cout << "Enter the arifmetic expression: \n";
-    cin >> line;
+    cin >> expression;
     string error;
     while (true)
     {
         try
         {
-            for (int i = 0; i < line.size(); i++)
-            {
-                error += line[i];
-                if (line[i] == ' ')
-                {
-                    error += "Don't forget, you can't use spaces in your line";
-                    throw invalid_argument(error.c_str());
-                }
-            }
             int choice;
             cout << "Do you want to calculate? \n";
             cout << "1. Yes \n";
@@ -35,14 +25,15 @@ int main()
             cin >> choice;
             if (choice == 1)
             {
-               TPostfix mathexp(line);
-               double res = mathexp.calculate();
-                cout << "The result: " << res << "\n";
+                TPostfix mathexp(expression);
+                mathexp.toPostfix();
+                mathexp.calculate();
+                cout << "The result: " << mathexp.getResult() << "\n";
             }
             if (choice == 2)
             {
                 cout << "Enter the arifmetic expression: \n";
-                cin >> line;
+                cin >> expression;
             }
             if (choice == 3)
             {
