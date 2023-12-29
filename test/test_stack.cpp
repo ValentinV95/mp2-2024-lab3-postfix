e2cd3206033f6bc6d27b08db5da10c2830eed0d9
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-12-29 10:10:08 +0300

    Update test_stack.cpp

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,86 +2,86 @@
 
 #include "stack.h"
 #include <gtest.h>
-TEST(TStack, can_create_stack)
+TEST(Stack, can_create_stack)
 {
-	ASSERT_NO_THROW(TStack<int> stack);
+	ASSERT_NO_THROW(Stack<int> stack);
 }
 
-TEST(TStack, can_push_element_in_stack) 
+TEST(Stack, can_push_element_in_stack) 
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	ASSERT_NO_THROW(stack.push(123));
 }
 
-TEST(TStack, can_pop_element_from_stack) 
+TEST(Stack, can_pop_element_from_stack) 
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(123);
 	EXPECT_EQ(123, stack.pop());
 }
 
-TEST(TStack, isEmpty_is_correct) 
+TEST(Stack, isEmpty_is_correct) 
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	EXPECT_EQ(true, stack.isEmpty());
 }
 
-TEST(TStack, top_is_correct) 
+TEST(Stack, top_is_correct) 
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(123);
 	EXPECT_EQ(123, stack.top());
 }
 
-TEST(TStack, can_top) 
+TEST(Stack, can_top) 
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(123);
 	ASSERT_NO_THROW(stack.top());
 }
 
-TEST(TStack, pop_delete_element_on_the_top_of_stack)
+TEST(Stack, pop_delete_element_on_the_top_of_stack)
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(123);
 	stack.push(45);
 	stack.pop();
 	EXPECT_EQ(123, stack.pop());
 }
 
-TEST(TStack, throws_when_do_top_when_stack_is_empty) 
+TEST(Stack, throws_when_do_top_when_stack_is_empty) 
 {
 	TStack<int> stack;
 	ASSERT_ANY_THROW(stack.top());
 }
 
-TEST(TStack, can_pop) 
+TEST(Stack, can_pop) 
 {
 	TStack<int> stack;
 	stack.push(123);
 	ASSERT_NO_THROW(stack.pop());
 }
 
-TEST(TStack, throws_when_element_pop_from_empty_stack)
+TEST(Stack, throws_when_element_pop_from_empty_stack)
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(123);
 	stack.pop();
 	ASSERT_ANY_THROW(stack.pop());
 }
 
-TEST(TStack, stack_size_is_correct)
+TEST(Stack, stack_size_is_correct)
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(1);
-  stack.push(2);
-  stack.push(3);
+  	stack.push(2);
+  	stack.push(3);
 	ASSERT_EQ(3, stack.get_size());
 }
 
-TEST(TStack, pop_reduces_stack_size)
+TEST(Stack, pop_reduces_stack_size)
 {
-	TStack<int> stack;
+	Stack<int> stack;
 	stack.push(123);
 	stack.push(123);
 	stack.push(123);
