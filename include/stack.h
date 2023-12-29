ca0008dd9eb3117c5bd28c77fff84e25342e5176
Варя Шарутина <119804541+VarvaraSharutina@users.noreply.github.com>
2023-12-29 04:11:33 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,70 +1,70 @@
 #pragma once
 #include <iostream>
 using namespace std;
-template <class T>
+
+template<typename T>
 class Stack
 {
-private:
-    T* data;          // Указатель на массив элементов стека
-    size_t capacity;  // Общая ёмкость стека
-    size_t size;      // Текущее количество элементов в стеке
+	T* data;
+	size_t capacity;
+	int size=-1;
 public:
-    Stack()
-    {
-        data = new T[10];
-        capacity = 10;
-        size = 0;
-    }
-    bool isEmpty()
-    {
-        return size == 0;
-    }
-    T pop()
-    {
-        if (isEmpty())
-            throw invalid_argument("Stack is empty");
-        else
-            return data[--size];
-    }
-    T top()
-    {
-        if (isEmpty())
-            throw invalid_argument("Sorry, stack is empty");
-        else
-            return data[size - 1];
-    }
-    void push(T elem)
-    {
-        if (size == capacity)
-        {
-            T* tmp = new T[size * 2];
-            for (size_t i = 0; i < size; i++)
-            {
-                tmp[i] = data[i];
-            }
-            delete[] data;
-            data = tmp;
-            capacity = 2 * size;
-        }
-        data[size++] = elem;
-    }
-    size_t getSize()
-    {
-        return size;
-    }
-    void clear()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            data[i] = {};
-        }
-    }
-    ~Stack()
-    {
-        delete[] data;
-        data = nullptr;
-        capacity = 0;
-        size = 0;
-    }
+	Stack()
+	{
+		data = new T[10];
+		capacity = 10;
+	}
+	Stack(size_t n)
+	{
+		capacity = n;
+		data = new T[capacity];
+		size = -1;
+	}
+	~Stack()
+	{
+		delete[] data;
+		data = nullptr;
+	}
+	bool isEmpty()
+	{
+		return size == 0;
+	}
+	T top()
+	{
+		if (isEmpty())
+			throw invalid_argument("Stack is empty");
+		else
+			return data[size - 1];
+	}
+	T pop()
+	{
+		if (isEmpty())
+			throw invalid_argument("Stack is empty");
+		else
+			return data[--size];
+	}
+	void push(const T& elem)
+	{
+		if (++size == capacity)
+		{
+			T* tmp = new T[capacity * 2];
+			for (size_t i = 0; i < capacity; i++)
+			{
+				tmp[i] = data[i];
+			}
+			delete[] data;
+			data = tmp;
+			capacity = 2 * capacity;
+		}
+		data[size] = elem;
+	}
+	size_t getSize()
+	{
+		return size;
+	}
+	void clear()
+	{
+		size = -1;
+	}
 };
 
