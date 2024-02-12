2efc7ee633e5bd73385b3573cba7f68871e4d60e
SosnovskayaUlyana <119797588+SosnovskayaUlyana@users.noreply.github.com>
2024-02-12 10:23:47 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,18 @@
 // реализация пользовательского приложения
+  #include <iostream>
+#include <string.h>
+#include "stack.h"
+#include "arithmetic.h"
+using namespace std;
 
-int main()
-{
-  return 0;
+
+int main() {
+    string h;
+    cout << "enter the expression";
+    cin >> h;
+    TCalculator s(h);
+    cout << s.Calc();
+    return 0;
 }
+};
+
