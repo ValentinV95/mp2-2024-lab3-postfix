9152b4a9e8cb0cd71cbb171914850c80deac8176
SosnovskayaUlyana <119797588+SosnovskayaUlyana@users.noreply.github.com>
2024-02-13 10:19:17 +0300

    Update test_arithmetic.cpp

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,42 +1,42 @@
 #include "arithmetic.h"
 #include <gtest.h>
-TEST(TPostfixCreation, FromSingleNumber) {
-	TPostfix postfix("12345");
-	EXPECT_EQ("12345", postfix.get_infix());
-	ASSERT_NO_THROW(postfix.to_postfix());
+
+TEST(TCalc, FromSingleNumber) {
+	TCalc calc("12345");
+	EXPECT_EQ("12345", calc.get_infix());
+	ASSERT_NO_THROW(calc.to_postfix());
 }
 
-TEST(TPostfixCreation, FromValidExpression) {
-	TPostfix postfix("1+2+3+4+5");
-	ASSERT_NO_THROW(postfix.to_postfix());
+TEST(TCalc, FromValidExpression) {
+	TCalc calc("1+2+3+4+5");
+	ASSERT_NO_THROW(calc.to_postfix());
 }
 
-TEST(TPostfixCreation, FromValidVariable) {
-	TPostfix postfix("x1");
-	ASSERT_NO_THROW(postfix.to_postfix());
+TEST(TCalc, FromValidVariable) {
+	TCalc calc("x1");
+	ASSERT_NO_THROW(calc.to_postfix());
 }
 
-TEST(TPostfixCreation, FromVariableAndNumber) {
-	TPostfix postfix("x1+123");
-	ASSERT_NO_THROW(postfix.to_postfix());
+TEST(TCalc, FromVariableAndNumber) {
+	TCalc calc("x1+123");
+	ASSERT_NO_THROW(calc.to_postfix());
 }
 
-
-TEST(TPostfixCalculation, CalculateIsCorrect) {
-	TPostfix postfix("1+2+3+4+5");
+TEST(TCalc, CalculateIsCorrect) {
+	TCalc calc("1+2+3+4+5");
 	map<string, double> v;
-	postfix.to_postfix();
-	EXPECT_NEAR(15.0, postfix.calculate(v), 0.01);
+	calc.to_postfix();
+	EXPECT_NEAR(15.0, calc.calculate(v), 0.01);
 }
 
-TEST(TPostfixCalculation, CalculateIsCorrectTwo) {
-	TPostfix postfix("((2+3)*9-8)/5");
+TEST(TCalc, CalculateIsCorrectTwo) {
+	TCalc calc("((2+3)*9-8)/5");
 	map<string, double> v;
-	postfix.to_postfix();
-	EXPECT_NEAR(7.4, postfix.calculate(v), 0.01);
+	calc.to_postfix();
+	EXPECT_NEAR(7.4, calc.calculate(v), 0.01);
 }
 
-TEST(TPostfixErrorHandling, ThrowsWhenNumberIsDot) {
-	TPostfix postfix(".");
-	ASSERT_ANY_THROW(postfix.to_postfix());
+TEST(TCalc, ThrowsWhenNumberIsDot) {
+	TCalc calc(".");
+	ASSERT_ANY_THROW(calc.to_postfix());
 }
