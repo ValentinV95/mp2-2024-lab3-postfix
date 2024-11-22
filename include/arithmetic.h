a529dedb6ad3075f6972422b12ea40679d1166d5
Maksimka <maksimzagradskrv50910@gmail.com>
2024-11-23 00:02:50 +0300

    Some virtual functions added

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,6 +1,9 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
 #include <string>
+#include <exception>
+#include <stdexcept>
 #include "myvector.h"
+#include "stack.h"
 
 using namespace std;
 
@@ -8,27 +11,79 @@ class lexem {
 private:
 	string sym;
 	int pos;
+public:
 	lexem(string s = "", int p = -1) : sym(s), pos(p) {}
+	const string& getSym() const noexcept { return sym; }
+	const int& getPos() const noexcept { return pos; }
+	virtual int getPriority() = 0;
 };
 
 class operation : public lexem {
 private:
 	int priority;
-	static myVector<string> avaliable_operations;
-	static void fill_operations() {
-		avaliable_operations.push_back("+");
-		avaliable_operations.push_back("-");
-		avaliable_operations.push_back("~");
-		avaliable_operations.push_back("*");
-		avaliable_operations.push_back("/");
-		avaliable_operations.push_back("sin(");
-		avaliable_operations.push_back("cos(");
-		avaliable_operations.push_back("exp(");
-		avaliable_operations.push_back("pow(");
-		avaliable_operations.push_back("log(");
-		avaliable_operations.push_back("");
-		avaliable_operations.push_back("(");
-		avaliable_operations.push_back(")");
+	static myVector<string> availableOperations;
+	static myVector<int> priotities;
+	static myVector<int> operandsCount;
+	static void fillOperations() {
+		availableOperations.push_back("+");
+		availableOperations.push_back("-");
+		availableOperations.push_back("~");
+		availableOperations.push_back("*");
+		availableOperations.push_back("/");
+		availableOperations.push_back("sin(");
+		availableOperations.push_back("cos(");
+		availableOperations.push_back("exp(");
+		availableOperations.push_back("pow(");
+		availableOperations.push_back("log(");
+		availableOperations.push_back("(");
+		availableOperations.push_back(")");
+
+		priotities.push_back(1);
+		priotities.push_back(1);
+		priotities.push_back(3);
+		priotities.push_back(2);
+		priotities.push_back(2);
+		priotities.push_back(3);
+		priotities.push_back(3);
+		priotities.push_back(3);
+		priotities.push_back(3);
+		priotities.push_back(3);
+		priotities.push_back(3);
+		priotities.push_back(0);
+		priotities.push_back(0);
+
+		operandsCount.push_back(2);
+		operandsCount.push_back(2);
+		operandsCount.push_back(1);
+		operandsCount.push_back(2);
+		operandsCount.push_back(2);
+		operandsCount.push_back(1);
+		operandsCount.push_back(1);
+		operandsCount.push_back(1);
+		operandsCount.push_back(2);
+		operandsCount.push_back(1);
+		operandsCount.push_back(0);
+		operandsCount.push_back(0);
+	}
+public:
+	operation(const lexem& lex) : priority(-1) { 
+		fillOperations(); 
+		for (size_t i = 0; i < availableOperations.size(); ++i) {
+			if (lex.getSym() == availableOperations[i]) {
+				priority = priotities[i];
+				break;
+			}
+		}
+		if (priority == -1) throw invalid_argument(to_string(lex.getPos()));
+	}
+	bool static isOperation(const lexem& lex) {
+		for (size_t i = 0; i < availableOperations.size(); ++i)
+			if (lex.getSym() == availableOperations[i])
+				return true;
+		return false;
+	}
+	int getPriority() override {
+		return priority;
 	}
 };
 
@@ -44,6 +99,45 @@ class variable : public operand {
 
 };
 
-class RPN {
-
+class RPN { // reverse polish notation
+private:
+	myVector<lexem> infix;
+	myVector<lexem*> postfix;
+	void toRpn() {
+		myStack<lexem*> st;
+		for (size_t i = 0; i < infix.size(); ++i) {
+			if (infix[i].getSym() == "(") {
+				st.push(&infix[i]);
+			}
+			if (infix[i].getSym() == ")") {
+				st.pop();
+				if (st.isEmpty()) throw invalid_argument(to_string(infix[i].getPos())); // redundant check
+				while (st.top()->getSym() != "(") {
+					if (st.isEmpty()) throw invalid_argument(to_string(infix[i].getPos())); // redundant check
+					postfix.push_back(st.pop());
+				}
+				st.pop();
+			}
+			else if (operation::isOperation(infix[i])) {
+				while (!st.isEmpty() && (st.top()->getPriority() >= (&infix[i])->getPriority())) {
+					postfix.push_back(st.pop());
+				}
+				st.push(&infix[i]);
+			}
+			else {
+				postfix.push_back(&infix[i]);
+			}
+		}
+		while (!st.isEmpty()) {
+			if (st.top()->getSym() == "(") throw invalid_argument(to_string(st.top()->getPos())); // redundant check
+			postfix.push_back(st.pop());
+		}
+	}
+public:
+	RPN(const myVector<lexem>& s) : infix(s), postfix() { toRpn(); }
+	double calculate() {
+		double res = 0.0;
+		myStack<lexem*> st;
+		return res;
+	}
 };
--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -10,25 +10,9 @@ using namespace std;
 int main()
 {
 	string s;
-	double num;
-
-	try {
-		while (true) {
-			getline(cin, s);
-			std::ios_base::fmtflags f(cout.flags());
-			num = parser(s);
-			cout << num << endl;
-			cout << fixed << setprecision(20) << num << endl;
-			cout.flags(f);
-		}
-	}
-	catch (exception& e) {
-		int it = stoi(e.what());
-		cout << s.substr(0, it) << " ";
-		cout << "\033[31m" << s[it] << "\033[0m";
-		if (it >= s.length()) cout << " " << s.substr(it, s.length()) << endl;
-		else cout << " " << s.substr(it + 1, s.length()) << endl;
-	}
+	getline(cin, s);
+	cout << s << endl;
+	
 
 	return 0;
 }
