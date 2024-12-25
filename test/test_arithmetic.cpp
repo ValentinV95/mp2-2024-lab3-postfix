3a970c38b0f1d044e03566b213ab50f40a4498a2
Rasull2 <153918806+Rasull2@users.noreply.github.com>
2024-12-25 14:32:12 +0300

    лаба 3 Бабадаев Расул

--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,3 +1,44 @@
 // тесты для вычисления арифметических выражений
 
 #include <gtest.h>
+#include "Arithmetik.h"
+
+class ArithmetikTest : public ::testing::Test {
+protected:
+    Arithmetik arithmetik;
+
+    void SetUp() override {
+
+    }
+};
+
+TEST_F(ArithmetikTest, TestAddition) {
+    arithmetik.setVar(0, 5);
+    arithmetik.setVar(1, 3);
+    arithmetik.calculate();
+    EXPECT_DOUBLE_EQ(arithmetik.calculate(), 8);
+}
+
+TEST_F(ArithmetikTest, TestSubtraction) {
+    arithmetik.setVar(0, 10);
+    arithmetik.setVar(1, 4);
+    EXPECT_DOUBLE_EQ(arithmetik.calculate(), 6);
+}
+
+TEST_F(ArithmetikTest, TestMultiplication) {
+    arithmetik.setVar(0, 7);
+    arithmetik.setVar(1, 6);
+    EXPECT_DOUBLE_EQ(arithmetik.calculate(), 42);
+}
+
+TEST_F(ArithmetikTest, TestDivision) {
+    arithmetik.setVar(0, 8);
+    arithmetik.setVar(1, 2);
+    EXPECT_DOUBLE_EQ(arithmetik.calculate(), 4);
+}
+
+TEST_F(ArithmetikTest, TestPower) {
+    arithmetik.setVar(0, 2);
+    arithmetik.setVar(1, 3);
+    EXPECT_DOUBLE_EQ(arithmetik.calculate(), 8);
+}
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,4 +1,47 @@
 // тесты для стека
 
-#include "stack.h"
+#include "Stack.h"
 #include <gtest.h>
+
+
+class StackTest : public ::testing::Test {
+protected:
+    Stack<int> stack;
+
+    void SetUp() override {
+    }
+};
+
+TEST_F(StackTest, TestPushAndTop) {
+    stack.push(10);
+    EXPECT_EQ(stack.top(), 10);
+    stack.push(20);
+    EXPECT_EQ(stack.top(), 20);
+}
+
+TEST_F(StackTest, TestPop) {
+    stack.push(30);
+    stack.push(40);
+    EXPECT_EQ(stack.pop(), 40);
+    EXPECT_EQ(stack.top(), 30);
+}
+
+TEST_F(StackTest, TestIsEmpty) {
+    EXPECT_TRUE(stack.is_empty());
+    stack.push(50);
+    EXPECT_FALSE(stack.is_empty());
+}
+
+TEST_F(StackTest, TestIsFull) {
+    Stack<int> fullStack(1);
+    fullStack.push(60);
+    EXPECT_TRUE(fullStack.is_full());
+}
+
+TEST_F(StackTest, TestGetCount) {
+    EXPECT_EQ(stack.Getcount(), 0);
+    stack.push(70);
+    EXPECT_EQ(stack.Getcount(), 1);
+    stack.pop();
+    EXPECT_EQ(stack.Getcount(), 0);
+}
